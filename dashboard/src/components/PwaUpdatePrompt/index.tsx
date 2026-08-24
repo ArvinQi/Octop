1396e4f0b3d792181f93df7c8a679ce47a2dd009
yangbo <3186674656@qq.com>
2026-08-24 10:05:58 +0800

    fix(dashboard): wait for service worker activation before reload

--- dashboard/src/components/PwaUpdatePrompt/index.tsx
+++ dashboard/src/components/PwaUpdatePrompt/index.tsx
@@ -41,7 +41,7 @@ export default function PwaUpdatePrompt() {
     if (serviceMode) {
       updateApi.restartService().catch(() => {});
     }
-    applyUpdate();
+    await applyUpdate();
   };
 
   const handleDismiss = () => setVisible(false);
